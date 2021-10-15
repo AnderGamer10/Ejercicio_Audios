0f7a53335acda0f571c7494520a2b17abe6c3b94
AnderGamer10 <anderedondo10@hotmail.com>
2021-10-15 09:49:49 +0200

    haciendo scripts

--- audios.js
+++ audios.js
@@ -1,14 +1,24 @@
-var can1 = document.getRootNode("Audios/AC-DC.mp3");
-var audios = [can1];
+
+var i = 0;
+
+var audios = [
+  "audios/AC-DC.mp3",
+  "audios/Aretha.mp3",
+  "audios/Bob Marley.mp3",
+  "audios/Red Hot Chili Peppers.mp3"
+]
+
+var reproducir = new Audio();
 
 function activarSonido(){
-
+  reproducir = new Audio(audios[i]);
+  reproducir.play();
 }
 function cambiarCancionIzquierda(){
 
 }
 function pararSonido(){
-
+  reproducir.pause();
 }
 function cambiarCancionDerecho(){
 
@@ -19,3 +29,36 @@ function mutearVolumen(){
 function activarVolumen(){
 
 }
+
+
+function anterior(){
+  repro.pause();
+  if(i <= 0){
+    i = canciones.length - 1;
+  }else{
+    i--;
+  }
+  repro = new Audio(canciones[i]);
+  repro.play();
+}
+
+
+function siguiente(){
+  repro.pause();
+  if(i >= canciones.length){
+    i = 0;
+  }else{
+    i++;
+  }
+  repro = new Audio(canciones[i]);
+  repro.play();
+}
+
+function volUp(){
+
+}
+
+function volDown(){
+
+}
+
