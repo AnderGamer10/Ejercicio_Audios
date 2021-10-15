21e41624d3bcd5b5c960feafc9be58ddb3082775
AnderGamer10 <anderedondo10@hotmail.com>
2021-10-15 08:53:12 +0200

    primer commit

---
+++ audios.js
@@ -0,0 +1,21 @@
+var can1 = document.getRootNode("Audios/AC-DC.mp3");
+var audios = [can1];
+
+function activarSonido(){
+
+}
+function cambiarCancionIzquierda(){
+
+}
+function pararSonido(){
+
+}
+function cambiarCancionDerecho(){
+
+}
+function mutearVolumen(){
+
+}
+function activarVolumen(){
+
+}
